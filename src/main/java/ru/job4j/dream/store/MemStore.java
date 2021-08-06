735d3be9eca47c1f3bca893dadd640029ff8fd24
Dmitry <goldlike888@gmail.com>
2021-08-06 15:23:16 +0300

    PsqlStore, подготовка

---
+++ src/main/java/ru/job4j/dream/store/MemStore.java
@@ -0,0 +1,66 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SuppressWarnings("CheckStyle")
+public class MemStore {
+
+    private static final MemStore INST = new MemStore();
+    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private static AtomicInteger POST_ID = new AtomicInteger(4);
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
+    private MemStore() {
+        posts.put(1, new Post(1, "Junior Java Job", "New", "21.07.21"));
+        posts.put(2, new Post(2, "Middle Java Job", "New", "21.07.21"));
+        posts.put(3, new Post(3, "Senior Java Job", "New", "21.07.21"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
+    }
+
+    public static MemStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAllPosts() {
+        return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
+    }
+
+    public void save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(POST_ID.incrementAndGet());
+        }
+        posts.put(post.getId(), post);
+    }
+
+    public Post findPostById(int id) {
+        return posts.get(id);
+    }
+
+    public void save(Candidate candidate) {
+        if (candidate.getId() == 0) {
+            candidate.setId(CANDIDATE_ID.incrementAndGet());
+        }
+        candidates.put(candidate.getId(), candidate);
+    }
+
+    public Candidate findCandidateById(int id) {
+        return candidates.get(id);
+    }
+
+    public void deleteCandidateById(int id) {
+        candidates.keySet().removeIf(k -> k.equals(id));
+    }
+}
